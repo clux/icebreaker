ac102b5ae8efb2892fc6e3fc4e0a7e969739e575
clux <analsandblaster@gmail.com>
2013-09-01 18:15:03 +0100

    verify that the events fire

--- tests.js
+++ tests.js
@@ -1,11 +1,23 @@
 var test = require('tape');
 
 test("link", function (t) {
+  t.plan(2);
   var tag = document.createElement('ice-breaker');
   document.body.appendChild(tag);
 
+
   // can't test rendering yet as you can't query within the Shadow DOM
   setTimeout(function () {
-    t.end();
+    tag.addEventListener('hack', function () {
+      t.ok(true, 'hack triggered!');
+    });
+    tag.addEventListener('hackDone', function () {
+      t.ok(true, 'hack done!');
+    });
+
+    tag.hack();    
+    setTimeout(function () {
+      t.end();
+    }, 9000);
   }, 200);
 });
